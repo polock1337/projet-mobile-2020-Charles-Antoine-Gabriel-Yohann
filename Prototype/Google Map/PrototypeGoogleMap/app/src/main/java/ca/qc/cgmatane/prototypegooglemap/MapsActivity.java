d7992b59bbba2ec8465e604e60b3c9926a407ee4
Gabriel Gagné <1734220@cgmatane.qc.ca>
2020-11-15 02:47:12 -0500

    Prototype google map

---
+++ Prototype/Google Map/PrototypeGoogleMap/app/src/main/java/ca/qc/cgmatane/prototypegooglemap/MapsActivity.java
@@ -0,0 +1,56 @@
+package ca.qc.cgmatane.prototypegooglemap;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        Circle circle = mMap.addCircle(new CircleOptions()
+                .center(new LatLng(48.841960, -67.538009))
+                .radius(100)
+                .strokeColor(Color.BLACK)
+                .fillColor(0x220000FF)
+                .strokeWidth(3));
+
+    }
+}
