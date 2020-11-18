37bfca809d339960ead2bec3f51b0732c8867836
Antoine Bélanger <31995891+ThonyDelire@users.noreply.github.com>
2020-11-18 09:41:02 -0500

    Prototype Capture fini

--- Prototype/Capteurs/PreuveDeConceptCapteur/app/src/main/java/ca/qc/cgmatane/preuvedeconceptcapteur/MainActivity.java
+++ Prototype/Capteurs/PreuveDeConceptCapteur/app/src/main/java/ca/qc/cgmatane/preuvedeconceptcapteur/MainActivity.java
@@ -1,21 +1,30 @@
 package ca.qc.cgmatane.preuvedeconceptcapteur;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
+import android.content.ContentValues;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int PERMISSION_CODE = 1000;
+    private static final int IMAGE_CAPTURE_CODE = 1001;
     Button mCaptureButton;
     ImageView mImageView;
+
+    Uri image_uri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,11 +60,37 @@ public class MainActivity extends AppCompatActivity {
 
     }
     private void openCamera(){
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE, "Nouvelle Photo");
+        values.put(MediaStore.Images.Media.DESCRIPTION, "De la camera");
+        image_uri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
 
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, image_uri);
+        startActivityForResult(cameraIntent, IMAGE_CAPTURE_CODE);
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        //super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case PERMISSION_CODE:{
+                if(grantResults.length > 0 && grantResults[0] ==
+                PackageManager.PERMISSION_DENIED) {
+                    openCamera();
+                }
+                else{
+                    Toast.makeText(this, "Permission refuser...", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (resultCode == RESULT_OK){
+            mImageView.setImageURI(image_uri);
+        }
     }
 }
